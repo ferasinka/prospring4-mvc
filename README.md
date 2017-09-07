904c9a696525214f417578087b195e719e786d0c
ferasinka <iam@ferasinka.com>
2017-09-07 16:47:14 +0300

    First working view (contacts)

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 # ProSpring-MVC
 Source code with my modifications from Pro Spring 4th Edition (Chapter 16): http://a.co/8Tvb5w4
+
+---
+
+You'll need to download [ckeditor](https://github.com/ckeditor/ckeditor-releases) and [jqgrid](https://github.com/tonytomov/jqGrid) into:
+`/src/main/webapp/ckeditor/`
+`/src/main/webapp/jqgrid/`
---
+++ src/main/java/com/ferasinka/prospring4mvc/ContactController.java
@@ -0,0 +1,36 @@
+package com.ferasinka.prospring4mvc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/contacts")
+public class ContactController {
+	private static final Logger LOG = LoggerFactory.getLogger(ContactController.class);
+	private ContactService contactService;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String list(Model uiModel) {
+		LOG.info("Listing contacts");
+		
+		List<Contact> contacts = contactService.findAll();
+		
+		uiModel.addAttribute("contacts", contacts);
+		
+		LOG.info("No. of contacts: " + contacts.size());
+		
+		return "contacts/list";
+	}
+	
+	@Autowired
+	public void setContactService(ContactService contactService) {
+		this.contactService = contactService;
+	}
+}
